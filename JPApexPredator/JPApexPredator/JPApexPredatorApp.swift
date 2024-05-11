63a7d51f9e581342646d32c08d20786745b4ae53
esedagunduz <ebrargunduz07@gmail.com>
2024-05-11 21:09:19 +0300

    JP Apex Predators IOS App

---
+++ JPApexPredator/JPApexPredator/JPApexPredatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorApp.swift
+//  JPApexPredator
+//
+//  Created by ebrar seda gündüz on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
